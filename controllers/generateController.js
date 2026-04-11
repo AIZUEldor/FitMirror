cbc128ce439480e0b7278171a14457853453f2ee
AIZUEldor <eldor9406@icloud.com>
2026-04-11 23:25:41 +0500

    refactor: handle generate uploads with memory storage

--- controllers/generateController.js
+++ controllers/generateController.js
@@ -1,28 +1,86 @@
+const fs = require("fs/promises");
+const os = require("os");
+const path = require("path");
+const crypto = require("crypto");
+
 const { generateTryOnImage } = require("../services/aiService");
 const { replaceWithOptimizedImage } = require("../services/imageService");
 const { successResponse } = require("../utils/apiResponse");
 
-exports.generateTryOn = async (req, res, next) => {
+function createHttpError(message, statusCode = 500) {
+  const error = new Error(message);
+  error.statusCode = statusCode;
+  return error;
+}
+
+function getSingleFile(req, fieldName) {
+  const files = req.files?.[fieldName];
+
+  if (!Array.isArray(files) || files.length === 0) {
+    throw createHttpError(`${fieldName} majburiy`, 400);
+  }
+
+  return files[0];
+}
+
+function getExtensionFromMimeType(mimeType) {
+  const mimeToExtensionMap = {
+    "image/jpeg": ".jpg",
+    "image/png": ".png",
+  };
+
+  return mimeToExtensionMap[mimeType] || ".jpg";
+}
+
+async function writeBufferToTempFile(file) {
+  if (!file?.buffer || !Buffer.isBuffer(file.buffer)) {
+    throw createHttpError("Yuklangan fayl buffer formatida kelmadi", 400);
+  }
+
+  const extension = getExtensionFromMimeType(file.mimetype);
+  const tempFileName = `fitmirror-${crypto.randomUUID()}${extension}`;
+  const tempFilePath = path.join(os.tmpdir(), tempFileName);
+
+  await fs.writeFile(tempFilePath, file.buffer);
+
+  return tempFilePath;
+}
+
+async function safeDeleteFile(filePath) {
+  if (!filePath) return;
+
   try {
-    if (!req.files || !req.files.personImage || !req.files.clothImage) {
-      const error = new Error("personImage va clothImage majburiy");
-      error.statusCode = 400;
-      return next(error);
-    }
+    await fs.unlink(filePath);
+  } catch {
+  }
+}
 
-    const personFile = req.files.personImage[0];
-    const clothFile = req.files.clothImage[0];
+exports.generateTryOn = async (req, res, next) => {
+  let personTempPath;
+  let clothTempPath;
+  let optimizedPersonPath;
+  let optimizedClothPath;
 
-    const optimizedPersonPath = await replaceWithOptimizedImage(personFile.path);
-    const optimizedClothPath = await replaceWithOptimizedImage(clothFile.path);
+  try {
+    const personFile = getSingleFile(req, "personImage");
+    const clothFile = getSingleFile(req, "clothImage");
 
-    const result = await generateTryOnImage(optimizedPersonPath, optimizedClothPath);
+    personTempPath = await writeBufferToTempFile(personFile);
+    clothTempPath = await writeBufferToTempFile(clothFile);
+
+    optimizedPersonPath = await replaceWithOptimizedImage(personTempPath);
+    optimizedClothPath = await replaceWithOptimizedImage(clothTempPath);
+
+    const result = await generateTryOnImage(
+      optimizedPersonPath,
+      optimizedClothPath
+    );
 
     const cleanedResult = {
       mode: result.mode,
       resultImage: result.resultImage,
       resultUrl: result.resultUrl,
-      note: result.note
+      note: result.note,
     };
 
     return successResponse(
@@ -32,6 +90,17 @@ exports.generateTryOn = async (req, res, next) => {
       200
     );
   } catch (error) {
-    next(error);
+    return next(error);
+  } finally {
+    await safeDeleteFile(personTempPath);
+    await safeDeleteFile(clothTempPath);
+
+    if (optimizedPersonPath && optimizedPersonPath !== personTempPath) {
+      await safeDeleteFile(optimizedPersonPath);
+    }
+
+    if (optimizedClothPath && optimizedClothPath !== clothTempPath) {
+      await safeDeleteFile(optimizedClothPath);
+    }
   }
 };
