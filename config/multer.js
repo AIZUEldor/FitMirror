a4558c6ba2ab69d6f167f2889421dc21eb01ec86
AIZUEldor <eldor9406@icloud.com>
2026-04-11 23:15:53 +0500

    fix: switch to memory storage

--- config/multer.js
+++ config/multer.js
@@ -1,16 +1,7 @@
 const multer = require("multer");
-const path = require("path");
 
-// storage
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    const uniqueName = Date.now() + path.extname(file.originalname);
-    cb(null, uniqueName);
-  }
-});
+// memory storage
+const storage = multer.memoryStorage();
 
 // file filter
 const fileFilter = (req, file, cb) => {
@@ -27,7 +18,7 @@ const fileFilter = (req, file, cb) => {
 
 // limits
 const limits = {
-  fileSize: 10 * 1024 * 1024 // 2MB
+  fileSize: 10 * 1024 * 1024
 };
 
 const upload = multer({
